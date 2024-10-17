16ecde60b123826dbb369a3919a6fc8e3fecc225
Elida Lezcano <ELIDA LEZCANO@DESKTOP-MHH7HC9>
2024-10-17 15:15:15 -0300

    arrays

---
+++ Arrays/src/arrays/Arrays.java
@@ -0,0 +1,41 @@
+
+package arrays;
+
+import java.util.Scanner;
+
+public class Arrays {
+
+    public static void main(String[] args) {
+      
+       //declaracion
+        int vector [] = new int [4];
+       //asignacion manual
+       vector [0] = 2;
+       vector [1] = 32;
+       vector [2] = 50;      
+       vector [3] = 10;
+       
+       //asignacion dada por el usuario
+       
+       Scanner teclado = new Scanner (System.in);
+       for (int i =0; i<vector.length ; i++){
+           System.out.println("Ingrese el valor para el indice");
+           vector[i] = teclado.nextInt();
+       }
+       
+    
+          
+       //recorrido
+       int i;
+       for( i =0; i<vector.length ; i++){
+           System.out.println("Estoy en el indice: "+ i);
+           System.out.println("Tengo guardado un vector: "+ vector[i]);
+           System.out.println("----------------------------");
+       }
+       
+       
+       
+       
+    }    
+}
+   
