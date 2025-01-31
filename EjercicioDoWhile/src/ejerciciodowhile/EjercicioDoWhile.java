c832685ac28ba2fdee4881eca1cb5e2ebfd1dc2e
Elida Lezcano <ELIDA LEZCANO@DESKTOP-MHH7HC9>
2025-01-31 19:08:42 -0300

    Do While

---
+++ EjercicioDoWhile/src/ejerciciodowhile/EjercicioDoWhile.java
@@ -0,0 +1,16 @@
+
+package ejerciciodowhile;
+
+
+public class EjercicioDoWhile {
+
+    public static void main(String[] args) {
+       int contador =0;
+        do{
+           System.out.println("Estoy en la vuelta n°: "+contador+1);
+           contador=contador+1;
+       }while(contador <10);
+        
+    }
+    
+}
