8831e4baa16094caa1ee81da568926ff75563618
Elida Lezcano <ELIDA LEZCANO@DESKTOP-MHH7HC9>
2025-01-31 19:07:30 -0300

    5 ejercicios for

---
+++ CicloFOR/src/ciclofor/CicloFOR.java
@@ -0,0 +1,60 @@
+
+package ciclofor;
+
+import java.util.Scanner;
+
+public class CicloFOR {
+
+    public static void main(String[] args) {
+      //EJERCICIO 1
+        
+        /* int suma=0;
+       for(int contador=0;contador<=10;contador++){
+           System.out.println("Estoy en la vuelta n°: "+contador);
+           suma=5+contador;
+           if(suma>=7){
+               contador=11;
+           }
+           
+       }*/
+        
+        
+        //EJERCICIO 2
+       /* for(int i=0; i<35; i++){
+            System.out.println(i+1);
+        }*/
+       
+       
+       
+       //EJERCICIO 3
+       /*int numero;
+       String palabra = "";
+       Scanner teclado= new Scanner(System.in);
+        System.out.println("Ingrese el limite numerico a mostrar en pantalla");
+       numero=teclado.nextInt();
+       
+        
+       for(int i=0; i<numero; i++){
+           System.out.println(i+1);
+       }*/
+       
+     
+       //EJERCICIO 4
+       for(int i=200; i<=250; i++){
+           System.out.println(i++);
+       }
+       
+      
+      
+      //EJERCICIO 5
+       /* System.out.println("¡Cuenta regresiva para año nuevo!");
+        for(int i=11; i>1; i--){
+            System.out.println(i-1);
+        }
+      */
+       
+       
+       
+    }
+    
+}
